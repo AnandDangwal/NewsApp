ab2fa38e9722748e7fe985f4dc46deee11857464
Anand_Dangwal <dangwalanand2002@gmail.com>
2023-12-16 14:30:42 +0530

    Now you can read three different categories of news.

--- app/src/main/java/com/example/reado/MainActivity.kt
+++ app/src/main/java/com/example/reado/MainActivity.kt
@@ -1,91 +1,46 @@
 package com.example.reado
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import androidx.browser.customtabs.CustomTabsIntent
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.android.volley.Request
-import com.android.volley.toolbox.JsonObjectRequest
-import com.example.memeshare.MySingleton
 import com.example.reado.databinding.ActivityMainBinding
-import com.example.reado.databinding.ItemNewsBinding
 
-class MainActivity : AppCompatActivity(), NewsItemClicked {
-    private lateinit var binding : ActivityMainBinding
-    private lateinit var mAdapter : MyAdapter
+class MainActivity : AppCompatActivity(){
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val recyclerView : RecyclerView = binding.rvItemsList
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
-        fetchData()
-        mAdapter = MyAdapter(this,this)
-        recyclerView.adapter = mAdapter
+        val mostEmailedButton = binding.btn1
+        val mostSharedButton = binding.btn2
+        val mostViewedButton = binding.btn3
 
 
-    }
+        val mostEmailedArticles  = "https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=LhqfAOH1L1lfFPePgWPoK82oUYYBqLxe"
+        val mostSharedArticles  = "https://api.nytimes.com/svc/mostpopular/v2/shared/7/facebook.json?api-key=LhqfAOH1L1lfFPePgWPoK82oUYYBqLxe"
+        val mostViewedArticles  = "https://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=LhqfAOH1L1lfFPePgWPoK82oUYYBqLxe"
 
-    private fun fetchData() {
-        val url = "https://api.nytimes.com/svc/mostpopular/v2/emailed/7.json?api-key=LhqfAOH1L1lfFPePgWPoK82oUYYBqLxe"
+        val intent = Intent(this, RecyclerViewActivity::class.java)
 
-        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+        mostEmailedButton.setOnClickListener(){
+            intent.putExtra("News Url",mostEmailedArticles)
+            startActivity(intent)
+        }
 
-            {response ->
-                val newsJsonArray = response.getJSONArray("results")
+        mostSharedButton.setOnClickListener(){
+            intent.putExtra("News Url",mostSharedArticles)
+            startActivity(intent)
+        }
 
-                val newsArray = ArrayList<NewsApiAttributes>()
-
-                for (obj in 0 until newsJsonArray.length()){
-                    val newsJsonObject = newsJsonArray.getJSONObject(obj)
-                    val mediaArray = newsJsonObject.getJSONArray("media")
-
-                    if(mediaArray.length() != 0) {
-
-                        val mediaObject = mediaArray.getJSONObject(0)
-                        val metaMediaArray = mediaObject.getJSONArray("media-metadata")
-                        val newsImageObject = metaMediaArray.getJSONObject(2)
-
-                        val newsContent = NewsApiAttributes(
-                            newsJsonObject.getString("abstract"),
-                            newsJsonObject.getString("title"),
-                            newsJsonObject.getString("url"),
-                            newsImageObject.getString("url"),
-                            newsJsonObject.getString("byline")
-
-
-                        )
-                        newsArray.add(newsContent)
-                    }
-
-                }
-                mAdapter.updateNewsItem(newsArray)
-            },
-            {error ->
-//                Toast.makeText(this, "Api Calling failed!",Toast.LENGTH_LONG).show()
-                Log.e("API_CALL", "API Calling failed: ${error.message}")
-            }
-        )
-
-        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+        mostViewedButton.setOnClickListener(){
+            intent.putExtra("News Url",mostViewedArticles)
+            startActivity(intent)
+        }
 
     }
 
-    override fun onItemClicked(item: NewsApiAttributes) {
-        //Open article in browser.
-//        val uri = Uri.parse(item.url)
-//        val intent = Intent(Intent.ACTION_VIEW, uri)
-//        startActivity(intent)
 
-        //Open article inside the App.
-        val customTabsIntent = CustomTabsIntent.Builder().build()
-        customTabsIntent.launchUrl(this, Uri.parse(item.url))
-    }
+
 }
